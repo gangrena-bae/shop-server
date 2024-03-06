7d730b41fe230192d5cd82cae9f11d5bb593b146
gangrena-bae <woodman.mail.me@gmail.com>
2024-03-06 14:08:13 +0200

    ebanat

--- migrations/20240306044330-add-stock-column.js
+++ migrations/20240306044330-add-stock-column.js
@@ -3,16 +3,16 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // await queryInterface.addColumn("devices", "stock", {
-    //   type: Sequelize.BOOLEAN,
-    //   allowNull: false,
-    //   defaultValue: true,
-    // });
-    await queryInterface.addColumn("devices", "description", {
-      type: Sequelize.STRING,
+    await queryInterface.addColumn("devices", "stock", {
+      type: Sequelize.BOOLEAN,
       allowNull: false,
-      defaultValue: "",
+      defaultValue: true,
     });
+    // await queryInterface.addColumn("devices", "description", {
+    //   type: Sequelize.STRING,
+    //   allowNull: false,
+    //   defaultValue: "",
+    // });
   },
 
   async down(queryInterface, Sequelize) {
